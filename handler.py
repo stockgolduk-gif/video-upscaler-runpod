b375189ab56edc926849a853076745edaa8b01cf
stockgolduk-gif <stockgold.uk@gmail.com>
2026-01-08 07:12:41 +0000

    Update handler.py
    Start RunPod serverless handler loop

--- handler.py
+++ handler.py
@@ -1,14 +1,13 @@
 import runpod
-import json
 
 def handler(event):
-    print("Job received:")
-    print(json.dumps(event, indent=2))
-
     return {
         "status": "ok",
         "message": "Handler ran successfully",
         "input": event
     }
 
-runpod.serverless.start({"handler": handler})
+runpod.serverless.start({
+    "handler": handler
+})
+
